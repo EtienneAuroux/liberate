b93e39eead99a577adfb121c673ef0cde4ac9ac2
EtienneAuroux-ImpulseRadar <132366116+EtienneAuroux-ImpulseRadar@users.noreply.github.com>
2025-04-20 13:17:37 +0200

    Created bindings in flutter project

---
+++ flow_board/macos/Classes/flow_board.c
@@ -0,0 +1,3 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../flow_board.podspec for more information.
+#include "../../src/flow_board.c"
--- ideas_board/android/app/src/main/kotlin/com/example/ideas_board/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ideas_board
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
